9bfd6cdbbd8bc3f5509902a75473112c8c780631
E.Lochan <63242350+Lochan9@users.noreply.github.com>
2021-06-15 17:38:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
 import pygame
 x=pygame.init()
+
+#creating window    
 gamewindow = pygame.display.set_mode((1200,500))
 pygame.display.set_caption("MY First Game")
+
+#game specific variable
+exit_game = False
+game_over = False
+
+#creating game loop
+while not exit_game:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit_game = True
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                print("You have pressed right arrow key")
+
+
+pygame.quit()
+quit()
